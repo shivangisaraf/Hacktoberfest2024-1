d942da09862d204dfb01eafcdecf9d16785fe8ef
shivangisaraf <157287050+shivangisaraf@users.noreply.github.com>
2024-10-11 16:46:58 +0530

    Create coinChange(DpProblem).java
    this program solves the Coin Change Problem using dynamic programming. It counts the number of ways to sum given coin denominations to achieve a specified target amount. A 2D DP table is used to store intermediate results, avoiding redundant calculations. The time complexity is O(n * sum), where n is the number of coin types and sum is the target amount, ensuring efficient computation

---
+++ coinChange(DpProblem).java
@@ -0,0 +1,27 @@
+class CoinChange {
+
+    static int countWays(int coins[], int n, int sum) {
+        int[][] dp = new int[n + 1][sum + 1];
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1; // There is one way to make sum 0
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (coins[i - 1] <= j)
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[n][sum];
+    }
+
+    public static void main(String args[]) {
+        int coins[] = { 1, 2, 3 };
+        int n = coins.length;
+        int sum = 5;
+        System.out.println(countWays(coins, n, sum));
+    }
+}
